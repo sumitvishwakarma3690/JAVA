e34ec9221bf26e09d6f024ba6969f84edd665620
Sumit Vishwakarma <Sumit3690@users.noreply.github.com>
2025-01-23 17:51:12 +0530

    Initial commit

---
+++ Lec1/FirstClass.java
@@ -0,0 +1,17 @@
+class FirstClass{
+    public static void main(String[] args) {
+        // output
+        // System.out.println("Hello");
+        // System.out.print("Hello World with java\n");
+
+        // System.out.print("Hello World\n");
+        // System.out.print("Hello World with java");
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        System.out.println("*\n**");
+    }
+}
---
+++ Lec2/Main.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        String name = "Sumit";
+        System.out.println(name);
+        double price = 23.324;
+        System.out.println(price);
+        int b = 45;
+        int c = 323; 
+        int sum = b + c;
+        System.out.println(sum);
+        int diff = b -a;
+        System.out.println(diff);
+        int mul = a * b;
+        System.out.println(mul);
+
+    }
+}
