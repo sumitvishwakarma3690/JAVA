eafe3078cfb566ceda374630b59731d137227ffb
Sumit Vishwakarma <Sumit3690@users.noreply.github.com>
2025-01-25 00:14:47 +0530

    added loops

---
+++ Lec4/Loops.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Loops {
+    public static void main(String[] args) {
+        
+        //for LOOP
+        // for (int counter = 0; counter < 11; counter++) {
+        //     System.out.print(counter+" ");
+        // }
+
+        // for (int i = 0; i < 11; i++) {
+        //     System.out.print(i+" ");
+        // }
+
+        // while LOOP
+        // int i = 0;
+        // while (i < 11) {
+        //     System.out.println(i);
+        //     i = i + 1; // i++
+        // }
+
+        // do while LOOP
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i = i + 1;
+        // } while(i < 11);
+
+        int i = 12;
+        while ( i < 11) {
+            System.out.println("Sumit Bhai");
+        }
+
+        do {
+            System.out.println("Sumit Bhai");
+        }while(i < 11);    
+    }
+}
