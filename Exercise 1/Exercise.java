95660d48fc1f03bc5d63f05f18c306fbf72c4bbc
Sumit Vishwakarma <Sumit3690@users.noreply.github.com>
2025-02-01 00:53:18 +0530

    average

--- Exercise 1/Exercise.java
+++ Exercise 1/Exercise.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Exercise {
     // https://docs.google.com/document/d/1eEv8JMCr_ZBoE5JSsZxdT5Zq53vq388-EIbE_mi7QME/edit?tab=t.0 link to access the questions that are given to this exercise.
 
-    public static double average(int a, int b, int c) {
+    public static double average(double a, double b, double c) {
         return (a + b + c ) / 3;
     }
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextInt();
-        System.out.println("Enter number a "+ a);
-        int b = sc.nextInt();
-        System.out.println("Enter number b "+ a);
-        int c = sc.nextInt();
-        System.out.println("Enter number c "+ a);
+        double a = sc.nextDouble();
+        System.out.println("The number a "+ a);
+        double b = sc.nextDouble();
+        System.out.println("The number b "+ b);
+        double c = sc.nextDouble();
+        System.out.println("The number c "+ c);
         System.out.println("The average of entered no is " + average(a,b,c));
     }
 }
