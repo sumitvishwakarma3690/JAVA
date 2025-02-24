ff793e54e07161715b552690b08a5e1ac906ed65
Sumit Vishwakarma <162994296+sumitvishwakarma3690@users.noreply.github.com>
2025-02-25 00:05:46 +0530

    Arrays

--- 10-Arrays/Q1.java
+++ 10-Arrays/Q1.java
@@ -1,11 +1,19 @@
 import java.util.*;
 
 //Take an array as input from the user. Search for a given number x and print the index at which it occurs.
+// **** Linear Search ****
 public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int numbers[] = new int[size];
+        
+
+        //input 
+        for(int i=0; i<size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
         int x = sc.nextInt();
 
         // output 
